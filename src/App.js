586422caa4a99f5b1ef557c0f714ef42d2d57951
MadeneMeriem <meriem.mdn2005@gmail.com>
2025-08-17 14:21:25 +0100

    Testimonials and Contact

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Home from './components/home';
 import About from './components/about';
-import Work from  './work';
+import Work from  './components/work';
+import Testimonials from './components/testimonials';
+import Contact from './components/contact';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
-      <Work/>
+      <Work />
+      <Testimonials />
+      <Contact />
     </div>
   );
 }
